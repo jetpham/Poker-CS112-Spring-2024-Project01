aa1f929d00c4f8c1591175fd34498d4b65283e61
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-04-01 19:50:06 -0700

    skele

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+use anyhow::Context;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    address_pair: String,
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    let mut stream =
+        TcpStream::connect(args.address_pair).context("could not connect to socket")?;
+
+    let mut buffer = [0; 1500];
+    while let Ok(bytes) = stream.read(&mut buffer) {
+        // let actual = buffer[0..bytes].clone();
+        match todo!() {}
+    }
+
+    // while true {
+
+    // }
+
+    Ok(())
 }
