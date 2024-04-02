d3b925dce6cabd852cc8b29caf9cffdc357520e2
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-04-01 21:37:43 -0700

    parsing tcp messages and branching in main

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
-#[derive(Copy, Clone)]
 use nom::{
-    branch::alt, bytes::complete::{tag, take_till, take_until1}, combinator::value, IResult
+    branch::alt,
+    bytes::{
+        complete::{is_not, tag, take_till, take_until},
+        streaming::take,
+    },
+    character::complete::{alpha1, not_line_ending},
+    combinator::value,
+    IResult,
 };
 
 struct Card {
@@ -57,26 +63,102 @@ struct Round {
     players: Vec<(Card, Option<Card>)>,
 }
 
-#[derive(Clone)]
-enum ServerResponse <'a>{
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ResponseVariant {
     Login,
-    Bet(&'a str),
-    Status(&'a str),
-    Done(&'a str),
-    Misc(&'a str),
+    Bet,
+    Status,
+    Done,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ServerResponse<'a> {
+    pub variant: ResponseVariant,
+    pub contents: &'a str,
 }
 
 fn till_colon(i: &str) -> IResult<&str, &str> {
     take_till(|c| c == ':')(i)
 }
 
-fn server_response(i: &str) -> IResult<ServerResponse, &str> {
-    let (i, took) = till_colon(i)?;
+pub fn parse_response(i: &str) -> IResult<&str, ServerResponse> {
+    let (contents, took) = till_colon(i)?;
+    let (i, variant) = alt((
+        value(ResponseVariant::Login, tag("login")),
+        value(ResponseVariant::Bet, tag("bet1")),
+        value(ResponseVariant::Bet, tag("bet2")),
+        value(ResponseVariant::Status, tag("status")),
+        value(ResponseVariant::Done, tag("done")),
+    ))(took)?;
 
-    let (i, response) = alt(
-        value(ServerResponse::Login, tag("login")),
-        value(ServerResponse::Bet(i), tag("bet1")),
-    )
-    
-    todo!()
+    Ok((i, ServerResponse { variant, contents }))
+}
+
+#[test]
+fn test_recv() {
+    assert_eq!(
+        parse_response("login"),
+        Ok((
+            "",
+            ServerResponse {
+                variant: ResponseVariant::Login,
+                contents: ""
+            }
+        ))
+    );
+
+    assert_eq!(
+        parse_response("bet1:208:24:12:KS:10D:up:AD:8H:10D:QD:2C"),
+        Ok((
+            "",
+            ServerResponse {
+                variant: ResponseVariant::Bet,
+                contents: ":208:24:12:KS:10D:up:AD:8H:10D:QD:2C"
+            }
+        ))
+    );
+
+    assert_eq!(
+        parse_response("bet1:188:68:5:KS:10D:up:AS:8H:10D:QD:2C"),
+        Ok((
+            "",
+            ServerResponse {
+                variant: ResponseVariant::Bet,
+                contents: ":188:68:5:KS:10D:up:AS:8H:10D:QD:2C"
+            }
+        ))
+    );
+
+    assert_eq!(
+        parse_response("bet2:183:66:0:KS:10D:10S:up:AS:AQ:8H:6D:10D:4S:QD:JC:2C:4H"),
+        Ok((
+            "",
+            ServerResponse {
+                variant: ResponseVariant::Bet,
+                contents: ":183:66:0:KS:10D:10S:up:AS:AQ:8H:6D:10D:4S:QD:JC:2C:4H"
+            }
+        ))
+    );
+
+    assert_eq!(
+        parse_response("status:win:AS:AC:AD"),
+        Ok((
+            "",
+            ServerResponse {
+                variant: ResponseVariant::Status,
+                contents: ":win:AS:AC:AD"
+            }
+        ))
+    );
+
+    assert_eq!(
+        parse_response("done:explaination"),
+        Ok((
+            "",
+            ServerResponse {
+                variant: ResponseVariant::Done,
+                contents: ":explaination"
+            }
+        ))
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
+use std::process::exit;
 use std::str::from_utf8;
 
 use anyhow::Context;
 use clap::Parser;
+
+use three_claw_stud::*;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -18,15 +21,54 @@ fn main() -> anyhow::Result<()> {
 
     let mut buffer = [0; 1500];
     while let Ok(bytes) = stream.read(&mut buffer) {
-        // let actual = buffer[0..bytes].clone();
+        let Ok(resp) = from_utf8(&buffer) else {
+            buffer = [0; 1500];
+            continue;
+        };
 
-        let response = from_utf8(&buffer)?;
-        match todo!() {}
+        match parse_response(resp) {
+            Ok((
+                i,
+                ServerResponse {
+                    variant: ResponseVariant::Login,
+                    contents,
+                },
+            )) => {
+                stream
+                    .write_all("jetpham:Crusty".as_bytes())
+                    .context("could not write to socket!")?;
+            }
+            Ok((
+                i,
+                ServerResponse {
+                    variant: ResponseVariant::Bet,
+                    contents,
+                },
+            )) => {}
+            Ok((
+                i,
+                ServerResponse {
+                    variant: ResponseVariant::Status,
+                    contents,
+                },
+            )) => {}
+            Ok((
+                i,
+                ServerResponse {
+                    variant: ResponseVariant::Done,
+                    contents,
+                },
+            )) => {
+                println!("done{}", contents);
+                break;
+            }
+            Err(e) => {
+                // eprintln!("{}", e);
+            }
+        };
+
+        buffer = [0; 1500];
     }
-
-    // while true {
-
-    // }
 
     Ok(())
 }
