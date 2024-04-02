16cd04890bbe8f2737941c4856cdd726e312edeb
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-04-01 19:50:06 -0700

    skele

--- src/lib.rs
+++ src/lib.rs
@@ -62,3 +62,60 @@ enum ServerResponse {
 }
 
 pub mod deck;
+struct Card {
+    rank: Rank,
+    suit: Suit,
+    seen: bool,
+}
+
+enum Rank {
+    Ace,
+    King,
+    Queen,
+    Jack,
+    Ten,
+    Nine,
+    Eight,
+    Seven,
+    Six,
+    Five,
+    Four,
+    Three,
+    Two,
+}
+
+enum Suit {
+    Spade,
+    Club,
+    Heart,
+    Diamond,
+}
+
+enum HandType {
+    ThreeKind,
+    TwoPair,
+    HighCard,
+}
+
+enum Bet {
+    Bet(u16),
+    Fold,
+}
+
+struct Round {
+    chips: u16,
+    pot: u16,
+    bet: u16,
+    hole: Card,
+    up_first: Card,
+    up_second: Option<Card>,
+    players: Vec<(Card, Option<Card>)>,
+}
+
+enum ServerResponse {
+    Login,
+    Bet(String),
+    Status(String),
+    Done(String),
+    Misc(String),
+}
